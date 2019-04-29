95585075eda0c3466a626329e7c9f027f50c62f5
Seonggu Kim <seonggu.de.kim@gmail.com>
2019-04-29 14:46:08 +0900

    Initial Upload

---
+++ miditokey.py
@@ -0,0 +1,31 @@
+import mido
+import keyboard
+
+def keymapping(msg):
+    if msg.hex() == '90 0C 7F':
+        keyboard.press('z')
+    elif msg.hex() == '90 0C 00':
+        keyboard.release('z')
+    elif msg.hex() == '90 0D 7F':
+        keyboard.press('x')
+    elif msg.hex() == '90 0D 00':
+        keyboard.release('x')
+    elif msg.hex() == '90 0E 7F':
+        keyboard.press('c')
+    elif msg.hex() == '90 0E 00':
+        keyboard.release('c')
+    elif msg.hex() == '90 0F 7F':
+        keyboard.press('v')
+    elif msg.hex() == '90 0F 00':
+        keyboard.release('v')
+def run():
+    with mido.open_input() as inport:
+        choice = input()
+        if choice == 'q':
+            return True
+        else:
+            for msg in inport:
+                keymapping(msg)
+
+
+run()
